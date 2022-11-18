7e96a407bc6df537bffe3be5581954e056ae917d
embergrainify <embergrainify@proton.me>
2022-11-18 13:01:08 +0700

    create docs folder for github

--- next.config.js
+++ next.config.js
@@ -3,6 +3,18 @@ const withPWA = require('next-pwa')({
   dest: 'public',
 })
 
+const basePath = process.env.NODE_ENV === 'production'?'/pwa-nextjs':'';
+
 module.exports = withPWA({
-  // config
+  reactStrictMode: true,
+  swcMinify: true,
+  productionBrowserSourceMaps: true,
+  env: {
+    basePath: basePath,
+  },
+  basePath: basePath,
+  images: {
+    unoptimized: true,
+  },
+  optimizeFonts: false,
 })
