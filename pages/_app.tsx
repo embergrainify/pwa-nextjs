ea31856a55ee921406c30db443cae216ec1df9fe
embergrainify <embergrainify@proton.me>
2022-11-19 21:11:14 +0700

    add showOpenFilePicker

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -12,25 +12,29 @@ export default function MyApp({ Component, pageProps }: AppProps) {
           name="viewport"
           content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
         />
-        <meta name="description" content="Description" />
-        <meta name="keywords" content="Keywords" />
-        <title>Next.js PWA Example</title>
+        <meta name="description" content="Progressive Web App - Next.JS" />
+        <meta name="keywords" content="pwa nextjs" />
+        <title>pwa-nextjs</title>
 
-        <link rel="manifest" href="/manifest.json" />
+        <link rel="manifest" href="manifest.json" />
         <link
-          href="/icons/favicon-16x16.png"
+          href="icons/icon-16x16.png"
           rel="icon"
           type="image/png"
           sizes="16x16"
         />
         <link
-          href="/icons/favicon-32x32.png"
+          href="icons/icon-32x32.png"
           rel="icon"
           type="image/png"
           sizes="32x32"
         />
-        <link rel="apple-touch-icon" href="/apple-icon.png"></link>
-        <meta name="theme-color" content="#317EFB" />
+
+        {/* https://web.dev/learn/pwa/web-app-manifest/#icons-in-safari > a single non-transparent PNG icon sized at 180 by 180 pixels is usually used*/}
+        <link rel="apple-touch-icon" href="icons/icon-192x192.png"></link> 
+
+        {/* <meta name="theme-color" content="#317EFB" /> */}
+        <meta name="theme-color" content="#317EFB" media="(prefers-color-scheme: dark)" />
       </Head>
       <Component {...pageProps} />
     </>
